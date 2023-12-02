d6826a2ef050101e9771edad56456875f3aeb4ea
septianwijaya45 <septianwijaya45@gmail.com>
2023-12-03 02:22:16 +0700

    adding logout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link, Navigate, Outlet } from 'react-router-dom'
 import { useStateContext } from '../contexts/ContextProvider';
+import axiosClient from '../axios-client';
 
 const DefaultLayout = () => {
-  const { user, token } = useStateContext();
+  const { user, setUser, token, setToken } = useStateContext();
 
   if(!token){
     return <Navigate to="/login" />
   }
 
-  const onLogout = (e) => {
-      e.preventDefault();
+  const onLogout = (ev) => {
+      ev.preventDefault();
+
+      axiosClient.post("/logout").then(() => {
+          setUser({});
+          setToken(null);
+      });
   };
+
+  useEffect(() => {
+      axiosClient.get("/user").then(({ data }) => {
+          setUser(data);
+      });
+  }, []);
 
   return (
       <div>
