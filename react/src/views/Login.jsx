b419e45d9c0b73bac5436c23cbb5bee3bd97c257
septianwijaya45 <septianwijaya45@gmail.com>
2023-12-03 00:11:37 +0700

    login & signup layouts

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+    };
 
-export default Login
+    return (
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Login into your account</h1>
+
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <button className="btn btn-block">Login</button>
+                    <p className="message">
+                        Not registered?{" "}
+                        <Link to="/signup">Create an account</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
